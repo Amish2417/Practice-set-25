f283c4ee39995941bac9a5d8eec361fe9c185a38
Amish2417 <anishraj2417>
2024-07-23 17:42:32 +0530

    Practice-Set-Class-24

---
+++ index.js
@@ -0,0 +1,105 @@
+// 1
+
+// let num = 0;
+// if(num>0){
+//   console.log("Number is Positive");
+// } else if(num<0){
+//   console.log("Number is Negative");
+// }else{
+//   console.log("Number is Zero");
+// }
+
+// 2
+
+// let age = 30;
+// let ticketPrice = 0;
+// let weekday1 = "Saturday";
+// let weekday2 = "Sunday";
+// let day = "Tuesday";
+// if(age<13){
+//   console.log("Free Ticket")
+// }else if(age >= 13 && age<=65){
+//   if(day == weekday1 || day == weekday2){
+//     ticketPrice += 500;
+//   }
+//   else{
+//     ticketPrice += 300;
+//   }
+
+// }
+// else{
+//   ticketPrice += 250
+// }
+// console.log(`Total price of the ticket is ${ticketPrice}`);
+
+// 3
+
+// let marks = 25;
+// if(marks>=90){
+//   console.log("Grade : A");
+// }else if(marks>=80 && marks<=89){
+//   console.log("Grade : B");
+// } else if(marks>=70 && marks<=79){
+//   console.log("Grade : C");
+// } else if(marks>=60 && marks<=69){
+//   console.log("Grade : D");
+// } else{
+//   console.log("Grade : F");
+
+// }
+
+// 4
+
+// let status = "";
+// let age = 10;
+// status = age>=18 ? "Major" : "Minor";
+// console.log(status);
+
+// 5
+
+// let day = "Friday";
+//   switch(day){
+//     case "Monday" :
+//       console.log("Happy Monday!");
+//       break;
+//       case "Tuesday" :
+//       console.log("Terrific Tuesday!");
+//       break;
+//       case "Wednesday" :
+//       console.log("Wonderful Wednesday!");
+//       break;
+//       case "Thursday" :
+//       console.log("Thirriving Thursday!");
+//       break;
+//       case "Friday" :
+//       console.log("Fun Friday!");
+//       break;
+//       case "Sarturday" :
+//       console.log("Super Saturday");
+//       break;
+//       case "Sunday" :
+//       console.log("Serene Sunday!");
+//       break;
+//       default:
+//         console.log("Invalid Day!")
+//   }
+
+// 6
+
+// let age = 25;
+// let income = 50000;
+// if(age<=13 || age>=65){
+//   if(income)
+
+// }
+ 
+
+// 7
+
+// let userInput ;
+// let defaultValue = "No Input Provided"
+// let output = userInput || defaultValue;
+// console.log(output);
+
+
+
